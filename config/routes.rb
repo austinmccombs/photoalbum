fe88c2727c140acddd113f5206fce24a87d06030
Austin McCombs <austinmccombs22@gmail.com>
2021-03-28 16:54:13 -0600

    trying to get rake routes to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :albums
-  get 'pages/about'
+  resources :albums, only: [:index, :show]
+  get 'about', to: 'pages#about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
